f5c9123ba054d9ac053252869f4fd1c889ebc60d
Sergio <srms98@gmail.com>
2023-09-06 17:32:09 +0200

    Fix total pago
    mejoras en estilos y arreglos interfaz y sumatorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { HeaderComponent } from './shared/components/header/header.component'
-import { FooterComponent } from './shared/components/footer/footer.component'
-import { HomeComponent } from './pages/home/home.component'
-import { EncargoComponent } from './pages/encargo/encargo.component'
-import { EncargoCardComponent } from './shared/components/encargo-card/encargo-card.component'
-import { FormsModule } from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EncargoComponent } from './pages/encargo/encargo.component';
+import { EncargoCardComponent } from './shared/components/encargo-card/encargo-card.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    EncargoComponent,
-    EncargoCardComponent
-  ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        HomeComponent,
+        EncargoComponent,
+        EncargoCardComponent
+    ],
+    imports: [BrowserModule, AppRoutingModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
